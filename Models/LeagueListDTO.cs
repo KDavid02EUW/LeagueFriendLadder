4a1198d26237dbd4ae8393d4a039dfc1d4d1887c
KDavid02 <kassa.david2002@gmail.com>
2026-03-13 13:18:36 +0100

    temp branch

--- Models/LeagueListDTO.cs
+++ Models/LeagueListDTO.cs
@@ -2,15 +2,15 @@
 {
     public class LeagueListDTO
     {
-        public string Name { get; set; } = string.Empty;
-        public string Tier { get; set; } = string.Empty;
-        public int LeaguePoints { get; set; } = 0;
-        public string Rank { get; set; } = string.Empty;
-        public double Wins { get; set; } = 0;
-        public double Losses { get; set; } = 0;
+        public string Name { get; set; }
+        public string Tier { get; set; }
+        public int LeaguePoints { get; set; }
+        public string Rank { get; set; }
+        public double Wins { get; set; }
+        public double Losses { get; set; }
 
         public double Winrate = 0;
-        public string Region { get; set; } = string.Empty;
+        public string Region { get; set; }
 
         public LeagueListDTO() {
             this.Winrate = Math.Round(this.Wins / (this.Wins + this.Losses) * 100);
