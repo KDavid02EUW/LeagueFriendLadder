cc0165e89eaedd30c34e4ef2cfcf5df0aab78e33
KDavid02 <kassa.david2002@gmail.com>
2026-05-07 15:20:49 +0200

    Removed timestamp on Blue team header in detailed view, fixed table spacing

--- LeagueFriendLadder.Api/obj/Debug/net8.0/LeagueFriendLadder.Api.AssemblyInfo.cs
+++ LeagueFriendLadder.Api/obj/Debug/net8.0/LeagueFriendLadder.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeagueFriendLadder.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b0696ac69a59a4b60fcafecbf7b50bbc50e203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dab620baf171c0ea16f2c4d49a22180516f0248")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeagueFriendLadder.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeagueFriendLadder.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
